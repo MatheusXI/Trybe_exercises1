127310f8a0406698157cbd0d7a54a59984f34d4b
Matheus <matheus.castro.macedo.xi@gmail.com>
2021-07-22 18:29:41 -0300

    Fim!

--- Dia-8.2/EX.8.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-// escreva greet abaixo
-const greet = (nome, msg = 'Hi') => `${msg} ${nome}`;
-
-assert.strictEqual(greet('John'), 'Hi John');
-assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
-assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');
